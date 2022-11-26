33e747649c8ba3b6427962064b61727a054c9c25
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-11-26 19:10:56 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -15,6 +15,7 @@ from telethon import events
 from telethon.tl.custom import Message
 from telethon.tl.types import MessageService
 from telethon.errors import MessageDeleteForbiddenError, MessageNotModifiedError
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 #CLIENTS
 bot = TelegramClient("TestBot", Config.API_ID, Config.API_HASH).start(bot_token=Config.BOT_TOKEN)
@@ -38,6 +39,7 @@ for sudo_id in sudo_users:
 	SUDOS.append(sudo_id)
 LOG_CHAT = Config.LOG_CHAT or -1001568226560
 agents = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0', 'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5', 'Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5', 'Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15']
+scheduler = AsyncIOScheduler()
 
 #FUNCs
 def get_db(variable, cn="MAIN"):
