dd7e6df0382fb1322318852b0c7107ce654a91aa
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-11-28 15:05:36 +0530

    Update api.py

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ async def root(request):
     return web.Response(text="Online!")
 
 app = web.Application()
-app.add_routes([web.get("/"), root])
+app.add_routes([web.get("/", root)])
 
 if __name__ == "__main__":
     web.run_app(app, port=Config.PORT)
