e9cc31ceacc9c7e78a560738cd6311385df8aacd
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-12-14 20:51:41 +0530

    Update fast_telethon.py

--- plugins/utils/fast_telethon.py
+++ plugins/utils/fast_telethon.py
@@ -418,24 +418,7 @@ def humanbytes(size):
     elif isinstance(size, float):
         size = f"{size:.2f}{unit}B"
     return size
-def time_formatter(milliseconds):
-    minutes, seconds = divmod(int(milliseconds / 1000), 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    weeks, days = divmod(days, 7)
-    tmp = (
-        ((str(weeks) + "w:") if weeks else "")
-        + ((str(days) + "d:") if days else "")
-        + ((str(hours) + "h:") if hours else "")
-        + ((str(minutes) + "m:") if minutes else "")
-        + ((str(seconds) + "s") if seconds else "")
-    )
-    if not tmp:
-        return "0 s"
 
-    if tmp.endswith(":"):
-        return tmp[:-1]
-    return tmp
 def numerize(number):
     if not number:
         return None
