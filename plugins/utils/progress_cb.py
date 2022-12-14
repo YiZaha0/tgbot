8ca9dc8cf29f545cf70bfe0c2b7666e7ab07070e
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-12-14 20:52:06 +0530

    Update progress_cb.py

--- plugins/utils/progress_cb.py
+++ plugins/utils/progress_cb.py
@@ -1,6 +1,24 @@
 import time
 import math
-from .fast_telethon import time_formatter
+
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (
+        ((str(weeks) + "w:") if weeks else "")
+        + ((str(days) + "d:") if days else "")
+        + ((str(hours) + "h:") if hours else "")
+        + ((str(minutes) + "m:") if minutes else "")
+        + ((str(seconds) + "s") if seconds else "")
+    )
+    if not tmp:
+        return "0 s"
+
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    return tmp
 
 def humanbytes(size):
     if not size:
