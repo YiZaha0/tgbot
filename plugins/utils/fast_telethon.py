5fafcb1e376173c9d992113ed831709a72b1d9d2
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-12-14 20:17:24 +0530

    Update fast_telethon.py

--- plugins/utils/fast_telethon.py
+++ plugins/utils/fast_telethon.py
@@ -45,6 +45,8 @@ from telethon.tl.types import (
     TypeInputFile,
 )
 
+from .progress_cb import progress
+
 log: logging.Logger = logging.getLogger("_FastTelethon")
 
 TypeLocation = Union[
@@ -447,43 +449,7 @@ def numerize(number):
         number = f"{number:.2f}{unit}"
     return number
 
-No_Flood = {}
-async def progress(current, total, event, start, type_of_ps, file_name=None):
-    now = time.time()
-    if No_Flood.get(event.chat_id):
-        if No_Flood[event.chat_id].get(event.id):
-            if (now - No_Flood[event.chat_id][event.id]) < 1.1:
-                return
-        else:
-            No_Flood[event.chat_id].update({event.id: now})
-    else:
-        No_Flood.update({event.chat_id: {event.id: now}})
-    diff = time.time() - start
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        time_to_completion = round((total - current) / speed) * 1000
-        progress_str = "`[{0}{1}] {2}%`\n\n".format(
-            "".join("●" for i in range(math.floor(percentage / 5))),
-            "".join("" for i in range(20 - math.floor(percentage / 5))),
-            round(percentage, 2),
-        )
 
-        tmp = (
-            progress_str
-            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
-                humanbytes(current),
-                humanbytes(total),
-                humanbytes(speed),
-                time_formatter(time_to_completion),
-            )
-        )
-        if file_name:
-            await event.edit(
-                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file_name, tmp)
-            )
-        else:
-            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
 
 
 async def uploader(file, name, taime, event, msg):
