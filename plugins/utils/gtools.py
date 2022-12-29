8c62d5c139187d3e61706ab82ecb8f989d181524
YiZaha0 <119040122+YiZaha0@users.noreply.github.com>
2022-12-29 21:28:58 +0530

    Update gtools.py

--- plugins/utils/gtools.py
+++ plugins/utils/gtools.py
@@ -2,6 +2,7 @@ import os
 import json
 import asyncio
 import random
+import traceback
 import ffmpeg
 import aiohttp
 
@@ -19,55 +20,67 @@ class Entry:
 	def __repr__(self):
 		return json.dumps(self.__dict__, indent=4)
 	
-	async def extract_download_urls(self):
-		link = self.link
-		cookies = {"auth": "aplnqLxgJbgtaoyFayGHsnA8ndd8z0BnmuGGwYwDl8BgPk3udnmsQsbW%2B4jXcmkfayLPOTXcZHip799T%2FTkUyg%3D%3D", "gogoanime": "hg9f7phuvd6ccm79k51unu6c62"}
-		content = await req_content(link, cookies=cookies, headers={"User-Agent": random.choice(agents)})
-		soup = BeautifulSoup(content, "html.parser")
-		
-		url_container = soup.find("div", "cf-download") 
-		url_container = url_container.find_all("a") if url_container is not None else []
-		
-		urls = dict()
-		for item in url_container:
-			url = item["href"]
-			quality = item.text.strip().split("x")[1]
-			urls[quality] = url 
-		
-		return {"urls": urls, "Referer": "https://gogoplay1.com/"}
+	async def _download_urls(self):
+		required_qualities = {"480p", "720p", "1080p"}
+		result = await extract_download_urls(self.link)
+		urls = result["urls"]
+		if not urls and not required_qualities.issubset(set(urls)):
+			try:
+				result = await extract_xstream_urls(self.link)
+			except:
+				return 
+			urls = result["urls"]
+		if urls and required_qualities.issubset(set(urls)):
+			return result
 	
-	async def extract_source_urls(self):
-		link = self.link 
-		content = await req_content(link, headers={"User-Agent": random.choice(agents)})
-		soup = BeautifulSoup(content, "html.parser")
+async def extract_download_urls(link:str) -> dict:
+	link = self.link
+	cookies = {"auth": "aplnqLxgJbgtaoyFayGHsnA8ndd8z0BnmuGGwYwDl8BgPk3udnmsQsbW%2B4jXcmkfayLPOTXcZHip799T%2FTkUyg%3D%3D", "gogoanime": "hg9f7phuvd6ccm79k51unu6c62"}
+	content = await req_content(link, cookies=cookies, headers={"User-Agent": random.choice(agents)})
+	soup = BeautifulSoup(content, "html.parser")
 		
-		multi_container = soup.find("div", "anime_muti_link")
-		multi_container = multi_container.find_all("li") if multi_container is not None else []
+	url_container = soup.find("div", "cf-download") 
+	url_container = url_container.find_all("a") if url_container is not None else []
 		
-		urls = dict()
-		for item in multi_container:
-			site = item.text.replace(item.span.text, "").strip()
-			link = item.a["data-video"]
-			urls[site] = link 
+	urls = dict()
+	for item in url_container:
+		url = item["href"]
+		quality = item.text.strip().split("x")[1] + "p"
+		urls[quality] = url 
+		
+	return {"urls": urls, "Referer": "https://gogoplay1.com/"}
+	
+async def extract_source_urls(link: str) -> dict:
+	content = await req_content(link, headers={"User-Agent": random.choice(agents)})
+	soup = BeautifulSoup(content, "html.parser")
+		
+	multi_container = soup.find("div", "anime_muti_link")
+	multi_container = multi_container.find_all("li") if multi_container is not None else []
+		
+	urls = dict()
+	for item in multi_container:
+		site = item.text.replace(item.span.text, "").strip()
+		link = item.a["data-video"]
+		urls[site] = link 
 		 
-		return urls
+	return urls
 
-	async def extract_xstream_urls(self):
-		download_urls = await self.extract_download_urls()
-		xstream_url = download_urls["Xstreamcdn"]
-		url_id = xstream_url.split("/")[-1]
-		xtream_url = f"https://fembed9hd.com/f/{url_id}"
-		code = f'''curl -A "uwu" -s -X POST "https://fembed9hd.com/api/source/{url_id}" -H "x-requested-with:XMLHttpRequest"'''
-		result = (await run_cmd(code))[0]
-		result = json.loads(result)["data"]
+async def extract_xstream_urls(link: str) -> dict:
+	download_urls = await extract_source_urls(link)
+	xstream_url = download_urls["Xstreamcdn"]
+	url_id = xstream_url.split("/")[-1]
+	xtream_url = f"https://fembed9hd.com/f/{url_id}"
+	code = f'''curl -A "uwu" -s -X POST "https://fembed9hd.com/api/source/{url_id}" -H "x-requested-with:XMLHttpRequest"'''
+	result = (await run_cmd(code))[0]
+	result = json.loads(result)["data"]
 		
-		urls = dict()
-		for i in result:
-			quality = i["label"]
-			url = i["file"]
-			urls[quality] = url
+	urls = dict()
+	for i in result:
+		quality = i["label"]
+		url = i["file"]
+		urls[quality] = url
 		
-		return {"urls": urls, "Referer": xstream_url}
+	return {"urls": urls, "Referer": xstream_url}
 		
 async def GogoFeed():
 	base = "https://gogoanime.dk/"
@@ -104,7 +117,7 @@ def gen_video_ss(file):
             .run(capture_stdout=True, capture_stderr=True)
         )
     except:
-        raise
+        traceback.print_exc()
     return out_filename
 
 def get_video_duration(path: str):
